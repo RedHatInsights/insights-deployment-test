a55f5088f611426800318c12fad55996af53c391
Ryan Long <rlong@redhat.com>
2018-07-11 13:14:38 -0400

    wrong name in travis.yml

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo "setup"
+
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
     openssl aes-256-cbc -K $encrypted_2eeb7a4fa1ff_key -iv $encrypted_2eeb7a4fa1ff_iv -in deploy_key.enc -out deploy_key -d
     chmod 600 deploy_key
