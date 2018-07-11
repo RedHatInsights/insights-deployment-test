d56ef47cac6e17f5cdf43a3e6591838d954d9114
Ryan Long <rlong@redhat.com>
2018-07-11 12:41:32 -0400

    moving to bash

---
+++ .travis/travis-setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if [ "${TRAVIS_BRANCH}" = "master" ]; then
+    openssl aes-256-cbc -K $encrypted_2eeb7a4fa1ff_key -iv $encrypted_2eeb7a4fa1ff_iv -in deploy_key.enc -out deploy_key -d
+    chmod 600 deploy_key
+    eval `ssh-agent -s`
+    ssh-add deploy_key
+fi
