f84add541e4a458a296093120dc7d65b324585ab
Ryan Long <rlong@redhat.com>
2018-07-11 12:57:19 -0400

    updating script

---
+++ .travis/branch-check.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# If current branch is master, push to ___
+if [ "${TRAVIS_BRANCH}" = "master" ]; then
+    chmod +x ./.travis/branch-check.sh
+fi
+
+# If current branch has stable, push to ___
+if [ "${TRAVIS_BRANCH}" = "master" ]; then
+    chmod +x ./.travis/branch-check.sh
+fi
+
+# Check if it is a pull request
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+    echo "Pull Request, no builds pushed"
+fi
