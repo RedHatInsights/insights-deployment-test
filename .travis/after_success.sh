31065653f30fc9de406e29bba7cbc37a181ae01d
Ryan Long <rlong@redhat.com>
2018-07-11 13:22:45 -0400

    moving before_install back out

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
 
-echo "Made it to the checks"
-
-echo ${REPO}
-
 # If current branch is master, push to ___
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
-    chmod +x ./travis-build.sh
+    travis-build.sh
 fi
 
 # If current branch has stable, push to ___
-if [ "${TRAVIS_BRANCH}" = "master" ]; then
-    chmod +x ./travis-build.sh
-fi
+
 
 # Check if it is a pull request
 if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
