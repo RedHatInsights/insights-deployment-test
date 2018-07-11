78e53cddb80a53382fca1396b31ad516b21f706c
Ryan Long <rlong@redhat.com>
2018-07-11 13:36:19 -0400

    reworking logic/

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -2,13 +2,12 @@
 
 # If current branch is master, push to ___
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
-    release.sh
+    # Check if it is a pull request
+    if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+        echo "Pull Request, not pushing a build"
+    else
+        .travis/release.sh
+    fi
 fi
 
 # If current branch has stable, push to ___
-
-
-# Check if it is a pull request
-if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-    echo "Pull Request, no builds pushed"
-fi
