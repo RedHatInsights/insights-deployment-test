eaeb152ba2327dad118e4cf1bceb43bda49ff02d
Ryan Long <rlong@redhat.com>
2018-07-11 13:02:17 -0400

    script update

--- .travis/branch-check.sh
+++ .travis/branch-check.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
+echo "Made it to the checks"
+
+echo ${REPO}
+
 # If current branch is master, push to ___
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
-    chmod +x ./branch-check.sh
+    chmod +x ./travis-build.sh
 fi
 
 # If current branch has stable, push to ___
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
-    chmod +x ./branch-check.sh
+    chmod +x ./travis-build.sh
 fi
 
 # Check if it is a pull request
